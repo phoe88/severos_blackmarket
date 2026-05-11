8f4a65634c38cdf05cb334f003f0e3fff3b0d178
Admin <admin@MacBook-Air-Admin.local>
2026-05-11 13:28:27 +0700

    membuat modifikasi

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
-
 <body>
-   <form action="register.php" method="POST">
-  <input type="text"     name="first_name" />
-  <input type="text"     name="last_name" />
-  <input type="email"    name="email" />
-  <input type="tel"      name="phone" />
-  <select               name="role">…</select>
-  <input type="password" name="password" />
-  <input type="password" name="confirm_password" />
-  <input type="checkbox" name="terms" value="1" />
-  <input type="checkbox" name="newsletter" value="1" />
-  <button type="submit">Create account</button>
-</form>
+
+    <form id="form-register" action="" method="POST">
+        <h1>Register</h1>
+        <div id="slogan">Sign Up. Load up. Stand out.</div>
+        <div id="subtitle">Create your account by filling in the iformation below</div>
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" placeholder="Enter your username (min. 8 characters)">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Enter your email">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Enter your password (min. 8 characters)">
+        <button type="submit"> <a href="register.php">Register</a></button>
+        <div id="option-login">Already have an account?<a href="login.php">Login here</a></div>
+    </form>
+
+
 
 </body>
-
 </html>
