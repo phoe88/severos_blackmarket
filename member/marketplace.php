b210e5f01721df171e3965914612a4411b2d1435
= <kamutaukanharusnya@gmail.com>
2026-05-15 07:15:04 -0700

    membuat perubahan pada member

---
+++ member/marketplace.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketplace</title>
+</head>
+
+<body>
+    <div id="header">
+        <h1>Marketplace</h1>
+        <p class="tagline">Only the strongest survive. Choose your weapons enforce your dominance</p>
+    </div>
+    <input type="search" name="search" id="search">
+    <div id="divider"></div>
+    <div class="container"></div>
+    <div class="filter-container">
+    <h1>Filters</h1>
+
+    </div>
+</body>
+
+</html>
