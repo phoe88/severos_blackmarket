479298e81e2e7d3f4e5172aa6293f69da2e96c07
= <kamutaukanharusnya@gmail.com>
2026-05-24 08:13:13 -0700

    membuat folder seperti semula

--- admin/admin_nav.php
+++
@@ -1,9 +0,0 @@
-<nav class="admin-nav">
-    <a href="dashboard.php">Dashboard</a>
-    <a href="weapon.php">Weapons</a>
-    <a href="weapontype.php">Weapon Types</a>
-    <a href="payment.php">Payments</a>
-    <form id="admin-logout-form" method="post" class="admin-logout-form">
-        <button type="submit" name="logout" class="admin-logout-button">Logout</button>
-    </form>
-</nav>
